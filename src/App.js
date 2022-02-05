0c71372888bd04cd63ef45bb2ef1ec4394bbaf64
ngodiingbentar <dewaqintoro@gmail.com>
2022-02-06 03:17:54 +0700

    layouting component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React from 'react';
-import './App.css';
+// import './App.css';
+import { Grid } from '@material-ui/core';
+
+import Details from './components/Details/Details';
+// import Main from './components/Main/Main';
+import useStyles from './styles';
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      dew
+    <div>
+      <Grid className={classes.grid} container spacing={0} alignItems="center" justify="center" style={{ height: '100vh'}}>
+        <Grid item xs={12} sm={4}>
+          <Details title="Income" />
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          {/* <Main /> */}
+          main
+        </Grid>
+        <Grid item xs={12} sm={4} >
+          <Details title="Expense" />
+        </Grid>
+        {/* <Grid item xs={12} sm={4} >
+          <Details title="Expense" />
+        </Grid> */}
+        {/* <PushToTalkButtonContainer>
+          <PushToTalkButton />
+        </PushToTalkButtonContainer> */}
+      </Grid>
     </div>
   );
 }
